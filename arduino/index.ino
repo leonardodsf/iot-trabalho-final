9aa951ddb9c939495c2a80ac084ec41724203894
yuri014 <yuriflores2012@gmail.com>
2021-06-15 21:52:59 -0300

    adding script for arduino without http

---
+++ arduino/index.ino
@@ -0,0 +1,24 @@
+int cm = 0;
+
+long readUltrasonicDistance(int triggerPin, int echoPin) {
+  pinMode(triggerPin, OUTPUT);
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  cm = 0.01723 * readUltrasonicDistance(3, 3);
+  if (cm < 100) {
+  	Serial.println(cm);
+  }
+  delay(5000);
+}
