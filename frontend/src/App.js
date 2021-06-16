2079e0b6fc677d61eaaedd876b89e9092dd85672
yuri014 <yuriflores2012@gmail.com>
2021-06-15 22:01:14 -0300

    disconnecting on unmount and mapping array

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,28 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
 function App() {
-  const [response, setResponse] = useState('');
-  
-  let socket;
+  const [response, setResponse] = useState([]);
 
   useEffect(() => {
+    const socket = io('ws://localhost:3333');
 
-    if (!socket) {
-      socket = new io('ws://localhost:3333');
-    }
-
-    socket.on('message', data => {
+    socket.on('message', (data) => {
       console.log(data);
-      setResponse(data);
+      setResponse([...response, data]);
     });
 
+    return () => socket.disconnect();
   }, [response, setResponse]);
 
   return (
     <div className="App">
       <div>
         <h1>e us guri</h1>
-        <span>
-          {response}
-        </span>
+        {response.map((value) => (
+          <span>{value}</span>
+        ))}
       </div>
     </div>
   );
